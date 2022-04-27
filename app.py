5f280790afd8c1c32d2891500f01e5ba132bb2ee
Ifeanyi-Egeolu <85946494+Ifeanyi-Egeolu@users.noreply.github.com>
2022-04-28 00:01:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ import subprocess
 class httpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path in ["/date", "/time"]:
-            self.path = "static" + self.path + ".html"
+            self.path = "dynamic" + self.path + ".html"
             return http.server.SimpleHTTPRequestHandler.do_GET(self)
         elif self.path == "/":
             self.path = "static/index.html"
             return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 
-conf = subprocess.run(["grep", "port=", "/var/tmp/mytime/mytime.conf"], stdout=subprocess.PIPE)
+conf = subprocess.run(["grep", "port=443", "/var/tmp/mytime/mytime.conf"], stdout=subprocess.PIPE)
 if b"=" not in conf.stdout:
     print("Could not read configuration file `/var/tmp/mytime/mytime.conf`")
     sys.exit(1)
